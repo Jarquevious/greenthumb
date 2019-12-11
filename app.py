529543133dcd65308fdb2d8f529c4632fe74747d
Jarquevious <jarquevious@gmail.com>
2019-12-10 18:10:46 -0800

    creating the routes

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
+from bson.objectid import ObjectId
+# Import pymongo and initialize client
+from pymongo import MongoClient
+client = MongoClient()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Return homepage."""
+    """Show all Appointments."""
     # change the original return statement you wrote to the one below
-    return render_template('home.html', msg='Flask is Cool!!')
+    return render_template('home.html')
+
+@app.route('/sign_up')
+def sign_up():
+    """Create a new appointment."""
+    return render_template('sign_up.html')
+
+@app.route('/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
